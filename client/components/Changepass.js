e992416bd8d9319efdb0a1381c5cff2f7458e600
Anabil <91933794+Anabil-Baruah@users.noreply.github.com>
2023-12-12 08:22:01 +0530

    final commit

--- client/components/Changepass.js
+++ client/components/Changepass.js
@@ -3,19 +3,47 @@ import { Text, View, SafeAreaView, StyleSheet, Image, FlatList, TouchableOpacity
 import { useCallback, useState, useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import styles from './styles/changepass.style';
+import { apiPost, apiGet } from './common/axios';
 
 SplashScreen.preventAutoHideAsync();
 
 export default Changepass = () => {
+    const [previousPassword, setPreviousPassword] = useState(""); // [1]
     const [newPassword, setNewPassword] = useState("");
     const [confirmNewPassword, setConfirmNewPassword] = useState("");
 
+
+    const submitNewPassword = async () => {
+        if (newPassword !== confirmNewPassword) {
+            alert("Password does not match")
+        } else {
+            const response = await apiPost('/users/changepassword', {
+                previousPassword: previousPassword,
+                newPassword: newPassword,
+            });
+            if (response.status === 200) {
+                alert("Password changed successfully")
+            } else {
+                alert(response.message)
+            }
+        }
+    }
     return (
         <View style={styles.container}>
             <View style={styles.changepassHeader}>
                 <Text style={styles.changepassText}>Change Password</Text>
             </View>
 
+            <View style={styles.newPassSearchContainer}>
+                <Text style={styles.newPassHeader}>Previous password:</Text>
+                <View style={styles.newPassSearchWrapper}>
+                    <TextInput
+                        style={styles.newPassSearchInput}
+                        value={previousPassword}
+                        onChangeText={(inputText) => setPreviousPassword(inputText)}
+                    />
+                </View>
+            </View>
             <View style={styles.newPassSearchContainer}>
                 <Text style={styles.newPassHeader}>New password:</Text>
                 <View style={styles.newPassSearchWrapper}>
@@ -41,13 +69,13 @@ export default Changepass = () => {
             <View style={styles.loginBtn}>
                 <TouchableOpacity
                     style={[styles.loginWrapper, {
-                        backgroundColor: !newPassword || !confirmNewPassword ? "#B7BCB5" : "black",
+                        backgroundColor: !previousPassword || !newPassword || !confirmNewPassword ? "#B7BCB5" : "black",
                         borderWidth: !newPassword || !confirmNewPassword ? 0 : 1,
                     }]}
 
-                    onPress={{}}
+                    onPress={submitNewPassword}
 
-                    disabled={!newPassword || !confirmNewPassword ? true : false}>
+                    disabled={!previousPassword || !newPassword || !confirmNewPassword ? true : false}>
                     <Text style={styles.loginButtonText}>Change Password</Text>
                 </TouchableOpacity>
             </View>
--- server/routers/user.js
+++ server/routers/user.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const user = require('../models/User')
+const User = require('../models/User')
+const bcrypt = require('bcrypt');
 
 router.route('/getUser')
     .get(async (req, res) => {
@@ -8,6 +9,61 @@ router.route('/getUser')
             res.json({ status: 200, message: result })
         } else {
             res.json({ status: 401, message: "No users found" })
+        }
+    })
+
+router.route('/changepassword')
+    .post(async (req, res) => {
+        // try {
+        //     const allUser = await user.find({})
+        //     const userFound = allUser[allUser.length - 1]
+        //     console.log(userFound)
+        //     console.log(req.body)
+
+        //     const result = await bcrypt.compare(req.body.previousPassword, userFound.password);
+        //     console.log(result)
+        //     if (result) {
+        //         const newHash = await bcrypt.hash(req.body.newPassword, 10);
+
+        //         userFound.password = newHash
+        //         const resultUserFound = await userFound.save()
+
+        //         if (resultUserFound)
+        //             res.json({ status: 200, message: "Password changed" })
+        //         else
+        //             res.json({ status: 400, message: "Some error occured" })
+        //     } else {
+        //         res.json({ status: 400, message: "Password dosen't match" })
+        //     }
+        // } catch (error) {
+        //     res.json({ status: 500, message: "Internal server error" })
+        // }
+        try {
+            const allUser = await User.find({})
+            const user = allUser[allUser.length - 1]
+
+            if (!user) {
+                return res.status(404).json({ status: 404, message: "User not found" });
+            }
+
+            const isPasswordMatch = await bcrypt.compare(req.body.previousPassword, user.password);
+            console.log(isPasswordMatch)
+
+            if (!isPasswordMatch) {
+                return res.json({ status: 400, message: "Current password is incorrect" });
+            }
+
+            // Hash the new password
+            const newHash = await bcrypt.hash(req.body.newPassword, 10);
+
+            // Update the user's password
+            user.password = newHash;
+            await user.save();
+
+            return res.json({ status: 200, message: "Password changed successfully" });
+        } catch (error) {
+            console.error(error);
+            return res.json({ status: 500, message: "Internal server error" });
         }
     })
 
